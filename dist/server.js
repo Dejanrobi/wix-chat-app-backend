c5c8b5ce1b4a50f37c9f438924c21ac37b634886
Dejanrobi <dejanrobi93@gmail.com>
2024-02-19 12:34:17 +0300

    Final Build Commit
    Final Build Commit

--- dist/server.js
+++ dist/server.js
@@ -16,9 +16,9 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-const openai_1 = require("openai");
 const behavior_directives_collection_1 = require("./behavior-directives-collection");
 const utils_1 = require("./utils");
+const generative_ai_1 = require("@google/generative-ai");
 dotenv_1.default.config();
 const PORT = process.env.PORT || 4000;
 const app = (0, express_1.default)();
@@ -27,6 +27,7 @@ app.use((0, cors_1.default)({
     origin(requestOrigin, callback) {
         const whitelist = [
             "http://localhost",
+            "https://wix-chat-app-backend.azurewebsites.net",
             `https://${process.env.WIX_APP_ID}.wix.run`,
         ];
         if (requestOrigin &&
@@ -57,41 +58,51 @@ app.get("/settings", function (req, res) {
         res.send({ behaviorDirective });
     });
 });
-app.post("/chat/product", function (req, res) {
+const history = [];
+app.post("/api/v1/chat/product", function (req, res) {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        const { instanceId } = (0, utils_1.authorizeWixRequest)(req);
-        const { messages, product } = req.body;
-        const configuration = new openai_1.Configuration({
-            apiKey: process.env.OPENAI_API_KEY,
-        });
-        const openai = new openai_1.OpenAIApi(configuration);
-        const completion = yield openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {
-                    role: "system",
-                    content: `You are Business Buddy, a chatbot that helps business owners with their businesses. 
-        You are tasked with helping a business owner with one of their products.
-        The business owner will chat with you about their product and you will give them advice on how to improve it.
+        const { prompt, product } = req.body;
+        const apiKey = (_a = process.env.GOOGLE_GEMINI_API_KEY) !== null && _a !== void 0 ? _a : '';
+        const configuration = new generative_ai_1.GoogleGenerativeAI(apiKey);
+        const generationConfig = {
+            stopSequences: ["red"],
+            maxOutputTokens: 400,
+            temperature: 0.9,
+            topP: 0.1,
+            topK: 16,
+        };
+        const safetySettings = [
+            {
+                category: generative_ai_1.HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: generative_ai_1.HarmBlockThreshold.BLOCK_ONLY_HIGH,
+            },
+            {
+                category: generative_ai_1.HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: generative_ai_1.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+            },
+        ];
+        const modelId = "gemini-pro";
+        const model = configuration.getGenerativeModel({ model: modelId, generationConfig, safetySettings });
+        const promptEngine = `You are Business Buddy, a chatbot that helps business owners with their businesses. 
+  You are tasked with helping a business owner with one of their products.
+  The business owner will chat with you about their product and you will give them advice on how to improve it.
 
-        The business owner has given the following directive as to how you should respond to their messages:
-        ${(0, behavior_directives_collection_1.getBehaviorDirective)(instanceId)}
-
-        The product is presented below as a JSON object:
-        ${product}`,
-                },
-                ...messages.map((message) => ({
-                    role: message.author === "Business Buddy"
-                        ? "assistant"
-                        : "user",
-                    content: message.text,
-                })),
-            ],
-            max_tokens: 2000,
-            n: 1,
-        });
-        res.send({ message: (_a = completion.data.choices[0].message) === null || _a === void 0 ? void 0 : _a.content });
+  The product is presented below as a JSON object:
+  ${product}
+  
+  Answer the following question from the business owner:
+  ${prompt}`;
+        try {
+            const result = yield model.generateContent(promptEngine);
+            const response = yield result.response;
+            const text = response.text();
+            res.send({ response: text });
+        }
+        catch (err) {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+        }
     });
 });
 app.listen(PORT, () => {
